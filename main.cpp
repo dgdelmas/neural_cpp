abd6f484371223a5b7441ccab9be8cb6e3f7cf98
dgdelmas <diegodelmastro@gmail.com>
2025-05-30 08:26:44 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(){
     layer L[depth]; // array of neurons
     L[0].set("dense", 32); // first layer, with 32 neurons
     L[1].set("dense", 16); // second layer, with 16 neurons
-    L[2].set("dense", 8); // third layer, with 8 neurons
+    L[2].set("dense", 8);  // third layer, with 8 neurons
     L[3].set("dense", 4);  // fourth layer, with 4 neurons
     L[4].set("dense", 2);  // fifth layer, with 2 neurons
 
@@ -26,26 +26,25 @@ int main(){
     int n_samp = 7776; // number of samples
     double ** X, ** Y;
     
-    X = new double*[n_samp];
-    Y = new double*[n_samp];
+    X = new double*[n_samp]; // declare predictor
+    Y = new double*[n_samp]; // declare response
     for(int i=0;i<n_samp;i++) X[i] = new double[n_in];
     for(int i=0;i<n_samp;i++) Y[i] = new double[2];
     
+    // uniform grid in [0,1]^5
     for(int i1=0;i1<6;i1++) for(int i2=0;i2<6;i2++) for(int i3=0;i3<6;i3++) for(int i4=0;i4<6;i4++) for(int i5=0;i5<6;i5++) X[i1+6*i2+36*i3+216*i4+1296*i5][0] = i1/5.;
     for(int i1=0;i1<6;i1++) for(int i2=0;i2<6;i2++) for(int i3=0;i3<6;i3++) for(int i4=0;i4<6;i4++) for(int i5=0;i5<6;i5++) X[i1+6*i2+36*i3+216*i4+1296*i5][1] = i2/5.;
     for(int i1=0;i1<6;i1++) for(int i2=0;i2<6;i2++) for(int i3=0;i3<6;i3++) for(int i4=0;i4<6;i4++) for(int i5=0;i5<6;i5++) X[i1+6*i2+36*i3+216*i4+1296*i5][2] = i3/5.;
     for(int i1=0;i1<6;i1++) for(int i2=0;i2<6;i2++) for(int i3=0;i3<6;i3++) for(int i4=0;i4<6;i4++) for(int i5=0;i5<6;i5++) X[i1+6*i2+36*i3+216*i4+1296*i5][3] = i4/5.;
     for(int i1=0;i1<6;i1++) for(int i2=0;i2<6;i2++) for(int i3=0;i3<6;i3++) for(int i4=0;i4<6;i4++) for(int i5=0;i5<6;i5++) X[i1+6*i2+36*i3+216*i4+1296*i5][4] = i5/5.;
 
+    // model to be learned
     for(int s=0;s<n_samp;s++) Y[s][0] = sin(X[s][0]+X[s][1]+X[s][2]+X[s][3]+X[s][4]);
     for(int s=0;s<n_samp;s++) Y[s][1] = 0.2*(cos(X[s][0]-X[s][1]+X[s][2]-X[s][3]+X[s][4])+X[s][2]-X[s][4]);
 
-
-
     N.train(X, Y, n_samp, /*batch_size=*/n_samp, /*learning_rate=*/.02, /*num_of_epochs=*/500);
 
-
-
+    // check that it worked
     for(int s=1000;s<1000+6;s++) cout << Y[s][0] << " ";
     cout << endl << "vs" << endl;
 
